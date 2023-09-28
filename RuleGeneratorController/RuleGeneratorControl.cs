da9129d81181ef97c966bbc4e1e3af6aee19a492
Andy Black <blackhandrew@gmail.com>
2023-09-28 15:08:50 -0700

    Setup for using ProgramData directory for web page

--- RuleGeneratorController/RuleGeneratorControl.cs
+++ RuleGeneratorController/RuleGeneratorControl.cs
@@ -19,13 +19,29 @@ namespace SIL.FLExTransRuleGenerator.Control
     public partial class RuleGeneratorControl : Form
     {
         const string kAppNAme = "FLExTransRuleGenerator";
+        string flexTransDir = "";
 
         public RuleGeneratorControl()
         {
             InitializeComponent();
+            CheckForPresenceInProgramData();
+
             wv2RuleEditor.WebMessageReceived += webView2_WebMessageReceived;
             wv2RuleEditor.Source = GetUriOfWebPage();
             wv2RuleEditor.Show();
+        }
+
+        private void CheckForPresenceInProgramData()
+        {
+            var programData = Environment.GetFolderPath(
+                Environment.SpecialFolder.CommonApplicationData
+            );
+            flexTransDir = Path.Combine(programData, "SIL", "FLExTransRuleGenerator");
+            if (!Directory.Exists(flexTransDir))
+            {
+                string testDataDir = GetTestDataDirectory();
+                Copy(testDataDir, flexTransDir);
+            }
         }
 
         private void webView2_WebMessageReceived(
@@ -39,14 +55,48 @@ namespace SIL.FLExTransRuleGenerator.Control
 
         private Uri GetUriOfWebPage()
         {
+            string webfile = Path.Combine(flexTransDir, "FLExTransRule.html");
+            Uri uri = new Uri(webfile);
+            return uri;
+        }
+
+        private static string GetTestDataDirectory()
+        {
             Uri uriBase = new Uri(Assembly.GetExecutingAssembly().CodeBase);
             var rootdir = Path.GetDirectoryName(Uri.UnescapeDataString(uriBase.AbsolutePath));
             int i = rootdir.LastIndexOf(kAppNAme);
             string basedir = rootdir.Substring(0, i);
             string testDataDir = Path.Combine(basedir, kAppNAme, "TestData");
-            string webfile = Path.Combine(testDataDir, "Test123.html");
-            Uri uri = new Uri(webfile);
-            return uri;
+            return testDataDir;
+        }
+
+        public static void Copy(string sourceDirectory, string targetDirectory)
+        {
+            var diSource = new DirectoryInfo(sourceDirectory);
+            var diTarget = new DirectoryInfo(targetDirectory);
+
+            CopyAll(diSource, diTarget);
+        }
+
+        // Following code taken from https://code.4noobz.net/c-copy-a-folder-its-content-and-the-subfolders/
+        // on 2023.09.28
+        public static void CopyAll(DirectoryInfo source, DirectoryInfo target)
+        {
+            Directory.CreateDirectory(target.FullName);
+
+            // Copy each file into the new directory.
+            foreach (FileInfo fi in source.GetFiles())
+            {
+                Console.WriteLine(@"Copying {0}\{1}", target.FullName, fi.Name);
+                fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
+            }
+
+            // Copy each subdirectory using recursion.
+            foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
+            {
+                DirectoryInfo nextTargetSubDir = target.CreateSubdirectory(diSourceSubDir.Name);
+                CopyAll(diSourceSubDir, nextTargetSubDir);
+            }
         }
     }
 }
